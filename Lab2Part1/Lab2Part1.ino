a26ed5a51f0e41e0d984c2a2648ba6a134c0f0a9
emustur <emustur@ilstu.edu>
2025-01-23 14:58:35 -0600

    While Loop Blink
    Inifinitely blinks since the variable is greater than 0

--- Lab2Part1/Lab2Part1.ino
+++ Lab2Part1/Lab2Part1.ino
@@ -7,12 +7,13 @@ void setup() {
   // initialize digital pin LED_BUILTIN as an output.
   pinMode(LED_BUILTIN, OUTPUT);
 
-  for(int i = 0; i < 5; i++){
-        digitalWrite(LED_BUILTIN, HIGH);  // turn the LED on (HIGH is the voltage level)
+  while(timesToBlink > 0){
+    digitalWrite(LED_BUILTIN, HIGH);  // turn the LED on (HIGH is the voltage level)
     delay(1000);                      // wait for a second
     digitalWrite(LED_BUILTIN, LOW);   // turn the LED off by making the voltage LOW
     delay(1000);
   }
+
 }
 
 // the loop function runs over and over again forever
