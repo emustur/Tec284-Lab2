42e09398bb5eadca5498d6f4187e89d653ff9868
emustur <emustur@ilstu.edu>
2025-01-23 14:55:55 -0600

    For Statement Blink

--- Lab2Part1/Lab2Part1.ino
+++ Lab2Part1/Lab2Part1.ino
@@ -7,14 +7,11 @@ void setup() {
   // initialize digital pin LED_BUILTIN as an output.
   pinMode(LED_BUILTIN, OUTPUT);
 
-  if(timesToBlink > 4){
-    digitalWrite(LED_BUILTIN, HIGH);  // turn the LED on (HIGH is the voltage level)
+  for(int i = 0; i < 5; i++){
+        digitalWrite(LED_BUILTIN, HIGH);  // turn the LED on (HIGH is the voltage level)
     delay(1000);                      // wait for a second
     digitalWrite(LED_BUILTIN, LOW);   // turn the LED off by making the voltage LOW
     delay(1000);
-  }
-  else{
-    digitalWrite(LED_BUILTIN, HIGH);
   }
 }
 
