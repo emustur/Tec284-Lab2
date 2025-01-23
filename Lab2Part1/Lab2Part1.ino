5702a4067ad4a33a9f352179e17ed23d3f13389b
emustur <emustur@ilstu.edu>
2025-01-23 14:51:32 -0600

    If Statement Blink

---
+++ Lab2Part1/Lab2Part1.ino
@@ -0,0 +1,23 @@
+
+// the setup function runs once when you press reset or power the board
+void setup() {
+  int timesToBlink = 1;
+  timesToBlink = timesToBlink + 2 * 4;
+
+  // initialize digital pin LED_BUILTIN as an output.
+  pinMode(LED_BUILTIN, OUTPUT);
+
+  if(timesToBlink > 4){
+    digitalWrite(LED_BUILTIN, HIGH);  // turn the LED on (HIGH is the voltage level)
+    delay(1000);                      // wait for a second
+    digitalWrite(LED_BUILTIN, LOW);   // turn the LED off by making the voltage LOW
+    delay(1000);
+  }
+  else{
+    digitalWrite(LED_BUILTIN, HIGH);
+  }
+}
+
+// the loop function runs over and over again forever
+void loop() {
+}
